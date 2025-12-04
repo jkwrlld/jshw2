6cd7c2dbdada09706be8ad9b481f08afb99c603b
jkwrlld <egordav729@gmail.com>
2025-12-04 19:26:59 +0200

    .

---
+++ js/hw.js
@@ -0,0 +1,14 @@
+// Створити змінну totalPrice для зберігання суми покупки в магазині. Застосувати знижку discount в розмірі 10% до цієї суми за допомогою оператора множення. Результат зберегти в змінній discountedPrice та вивести результат в консоль.
+const totalPrice = 100;
+const discount = 0.1;
+const discountedPrice = totalPrice - totalPrice * discount;
+console.log(discountedPrice);
+
+// Створити змінну const intString = "123" для зберігання рядка, який містить ціле число. Використати метод parseInt() для перетворення рядка у ціле число. Результат зберегти в змінній parsedIntта вивести результат в консоль.
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+// Створити змінну const floatString = "45.67"  для зберігання рядка, який містить числа з плаваючою комою. Використати метод parseFloat() для перетворення рядка у десяткове число. Результат зберегти в змінній parsedFloat та вивести результат в консоль.
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
